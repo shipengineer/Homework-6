0a4a2606e3f6e3a0e77941bc4540973a93685eaf
MarineIngenier <poeticcrane@gmail.com>
2022-09-19 14:26:53 +0300

    solve #41

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
 */
 //----------------------------------SOLUTION-----------------------------------
 
-
+/*
 System.Console.WriteLine("Введите числа через пробел");
             //       1              2              3               4
 int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
@@ -16,7 +16,7 @@ int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 3 -- используем метод проекции каждого разделенного элемента из string в int;
 4 -- превращаем полученный список в массив;
 */
-
+/*
  int count(int[] mas)
  {
     int i = 0;
@@ -27,4 +27,30 @@ int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
     return i;
  }
 System.Console.WriteLine($"В массиве {String.Join("|", array)} элементов больше нуля: {count(array)}");
+*/
+//----------------------------------Exercise #41------------------------------
+/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+//----------------------------------SOLUTION-----------------------------------
 
+double[] crossPt(double k1, double k2, double b1, double b2)
+{
+    double[] result = new double[2];
+   result[0] = (b1 - b2)/(k2-k1); // Вычисляем Х
+   result[1] = k1*result[0] + b1;      // Вычисляем Y
+    // double y2 = k2*x +b2;
+   return result;
+
+}
+System.Console.Write(" введите коэфициент k1 ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write(" введите коэфициент b1 ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write(" введите коэфициент k2 ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write(" введите коэфициент b2 ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double[] answer = crossPt(k1,k2,b1,b2);
+System.Console.WriteLine($"Точка пересечения заданных линейных уравнений Х = {answer[0]},  Y = {answer[1]}");
